60e146c4393d710ef2429cad2120795318b4a43d
Prince Shekhawat <shekhawatprince199@gmail.com>
2026-05-21 18:52:36 +0530

    Fix next config

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,4 +1,7 @@
-import { NextResponse } from "next/server";
+export const dynamic =
+  "force-dynamic";
+
+  import { NextResponse } from "next/server";
 
 import { supabase } from "@/lib/supabase";
 
